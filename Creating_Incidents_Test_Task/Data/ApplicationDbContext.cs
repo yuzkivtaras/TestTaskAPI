8eb8fd3d80c5709e1f204207215f74c49f61ac6f
yuzkivtaras <98391159+yuzkivtaras@users.noreply.github.com>
2023-02-20 00:25:14 +0200

    Update ApplicationDbContext.cs

--- Creating_Incidents_Test_Task/Data/ApplicationDbContext.cs
+++ Creating_Incidents_Test_Task/Data/ApplicationDbContext.cs
@@ -12,29 +12,5 @@ namespace Creating_Incidents_Test_Task.Data
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Incident> Incidents { get; set; }
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Contact>().HasData(new Contact
-        //    {
-        //        FirstName = "Tommy",
-        //        LastName = "Medina",
-        //        Email = "tommy98@gmail.com",
-        //    });
-
-        //    modelBuilder.Entity<Contact>().HasData(new Contact
-        //    {
-        //        FirstName = "Jack",
-        //        LastName = "Galloway",
-        //        Email = "john28@gmail.com",
-        //    });
-
-        //    modelBuilder.Entity<Contact>().HasData(new Contact
-        //    {
-        //        FirstName = "Mary",
-        //        LastName = "Smith",
-        //        Email = "mary198@gmail.com",
-        //    });
-        //}
     }
 }
